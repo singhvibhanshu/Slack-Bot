012e8a8a17e04533c325d5de55580df1021947f0
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-01-07 11:14:54 +0530

    setup and sending messages algo added

--- bot.py
+++ bot.py
@@ -1,19 +1,11 @@
-import slac
+import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask
-from slackeventsapi import SlackEventAdapter
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', app)
-
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
-client.chat_postMessage(channel="#test", text="Hey everyone! Welcome to this channel.")
-
-if __name__ == "__main__":
-    app.run(debug=True) #by default it is running on port 5000
+client.chat_postMessage(channel='#testing101', text="Hey there!")
